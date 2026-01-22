eec503897cbf915a622bb242c4f0be31a3687570
Ubai1234 <irfanubaidillah123@gmail.com>
2026-01-22 20:00:00 +0700

    updated
    updated

--- function.php
+++ function.php
@@ -1,5 +1,5 @@
 <?php
-$koneksi = mysqli_connect('localhost', 'root', '', 'cek');
+$koneksi = mysqli_connect('localhost', 'root', '', 'cek', 3307);
 
 if (mysqli_connect_errno()) {
     echo "Koneksi Database Gagal : " . mysqli_connect_error();
